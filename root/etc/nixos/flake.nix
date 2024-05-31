612a1e8fb51de70fc074c0f8df5c115283638b3b
= <=>
2024-06-01 09:11:13 +1000

    fix: put custom config before old config

--- root/etc/nixos/flake.nix
+++ root/etc/nixos/flake.nix
@@ -4,8 +4,8 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    # NixOS official package source, using the nixos-24.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -14,11 +14,7 @@
       system = "x86_64-linux";
 
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        /etc/nixos/configuration.nix
-
-	    ({ pkgs, ...}: {
+        ({ pkgs, ... }: {
           environment.systemPackages = with pkgs; [
             alacritty
             cron
@@ -27,25 +23,31 @@
             nixfmt
             openscad-unstable
             prusa-slicer
+            zoom-us
+            nixfmt-classic
             (vscode-with-extensions.override {
-              vscodeExtensions = with vscode-extensions; [
-                bbenoist.nix
-                # # overrides
-                # b4dm4n.nixpkgs-fmt
-                dbaeumer.vscode-eslint
-                eamodio.gitlens
-                esbenp.prettier-vscode
-                github.github-vscode-theme
-                pkief.material-icon-theme
-                pkief.material-product-icons
-                redhat.vscode-yaml
-                tamasfe.even-better-toml
-                vscodevim.vim
-              ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-              ];
+              vscodeExtensions = with vscode-extensions;
+                [
+                  bbenoist.nix
+                  # # overrides
+                  # b4dm4n.nixpkgs-fmt
+                  dbaeumer.vscode-eslint
+                  eamodio.gitlens
+                  esbenp.prettier-vscode
+                  github.github-vscode-theme
+                  pkief.material-icon-theme
+                  pkief.material-product-icons
+                  redhat.vscode-yaml
+                  tamasfe.even-better-toml
+                  vscodevim.vim
+                ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [ ];
             })
           ];
         })
+
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        /etc/nixos/configuration.nix
       ];
     };
   };
