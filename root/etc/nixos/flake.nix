152d51ea816c5cb41acdd39e236e48799f6e440e
= <=>
2024-05-31 23:20:22 +1000

    feat: add vscode extensions and pkgs to flake

--- root/etc/nixos/flake.nix
+++ root/etc/nixos/flake.nix
@@ -12,7 +12,7 @@
     # Please replace my-nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      
+
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
@@ -20,7 +20,30 @@
 
 	    ({ pkgs, ...}: {
           environment.systemPackages = with pkgs; [
+            alacritty
             cron
+            chromium
+            firefox
+            nixfmt
+            openscad-unstable
+            prusa-slicer
+            (vscode-with-extensions.override {
+              vscodeExtensions = with vscode-extensions; [
+                bbenoist.nix
+                # # overrides
+                # b4dm4n.nixpkgs-fmt
+                dbaeumer.vscode-eslint
+                eamodio.gitlens
+                esbenp.prettier-vscode
+                github.github-vscode-theme
+                pkief.material-icon-theme
+                pkief.material-product-icons
+                redhat.vscode-yaml
+                tamasfe.even-better-toml
+                vscodevim.vim
+              ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+              ];
+            })
           ];
         })
       ];
