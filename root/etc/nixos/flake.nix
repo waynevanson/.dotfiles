080d0f4354fc6ce8d41d64bbbc65906110642ec5
= <=>
2024-05-31 15:55:53 +1000

    chore: basic flake config

--- root/etc/nixos/flake.nix
+++ root/etc/nixos/flake.nix
@@ -1,22 +1,21 @@
 # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-with-flakes-enabled
-{
-  description = "A flake use for my environment";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.waynevanson = nixpkgs.lib.nixosSystem {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      
       modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
         ./configuration.nix
-      
-
-      ({ pkgs, ... }: {
-        packages = [
-          
-        ];
-      })
       ];
     };
   };
