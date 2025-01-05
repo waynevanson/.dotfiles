35821fda2fe868d3f7835dee783ca43fef4fca85
Wayne Van Son <waynevanson@gmail.com>
2025-01-05 13:05:39 +1100

    feat: add wsl to config

--- flake.nix
+++ flake.nix
@@ -1,35 +1,67 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
-    musnix  = { url = "github:musnix/musnix";};
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, musnix, ...}: let
-    system = "x86_64-linux";
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    nixos-wsl,
+    ...
+  }: let
+    system = builtins.currentSystem;
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
+    };
+    common = {
+      inherit system pkgs;
+      specialArgs = {inherit nixpkgs;};
     };
   in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       inherit system pkgs;
 
-
       modules = [
-        musnix.nixosModules.musnix
-        ({...}: { musnix.enable = true;})
         ./modules/docker.nix
         ./modules/gnome.nix
         ./modules/packages.nix
         ./modules/steam.nix
-        # ./modules/sway.nix
         ./modules/system.nix
 
         # Purely system related
         /etc/nixos/configuration.nix
       ];
 
-      specialArgs = { inherit nixpkgs; };
+      specialArgs = {inherit nixpkgs;};
+    };
+
+    nixosConfigurations.nixos-wsl = nixpkgs.lib.nixosSystem {
+      inherit system pkgs;
+
+      modules = [
+        nixos-wsl.nixosModules.default
+
+        ({pkgs, ...}: {
+          system.stateVersion = "24.05";
+          wsl.enable = true;
+          wsl.defaultUser = "waynevanson";
+
+          environment.systemPackages = with pkgs; [
+            corepack
+            curl
+            direnv
+            git
+            nodejs
+            vim
+            wget
+          ];
+        })
+      ];
+
+      specialArgs = {inherit nixpkgs;};
     };
   };
 }
